5a9ddbf02cce3aa279236d4635ab6c807dd5cbf5
Robert Deusser <iamthemuffinman@outlook.com>
2015-08-05 10:52:16 -0400

    Added JSON stuff

---
+++ json.go
@@ -0,0 +1,27 @@
+package httputils
+
+import (
+	"encoding/json"
+	"net/http"
+)
+
+// WriteJSONError writes a string as JSON encoded error
+func WriteJSONError(w http.ResponseWriter, code int, err string) {
+	w.WriteHeader(code)
+
+	WriteJSON(w, map[string]string{
+		"error": err,
+	})
+}
+
+// WriteJSON writes the given interface as JSON or returns an error
+func WriteJSON(w http.ResponseWriter, v interface{}) error {
+	b, err := json.MarshalIndent(v, "", "  ")
+	if err != nil {
+		return err
+	}
+
+	w.Header().Set("Content-Type", "application/json; charset=utf-8")
+	w.Write(b)
+	return nil
+}
